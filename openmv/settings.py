7dc430ef75cf37625a8365485e1f454b3b6f9753
Kevin Dunn <kgdunn@gmail.com>
2022-01-16 17:49:04 +0100

    Being more explicit in production with the settings from the .env file.

--- openmv/settings.py
+++ openmv/settings.py
@@ -9,26 +9,26 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
 from pathlib import Path
 
-from dotenv import dotenv_values, load_dotenv
+from dotenv import dotenv_values
 
-load_dotenv(dotenv_path="/var/django/openmv/")
+dotenv_file = "/var/django/openmv/.env"
+assert Path(dotenv_file).parent.exists(), f"{dotenv_file} directory does not exist"
+assert Path(dotenv_file).exists(), f"{dotenv_file} does not exist"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-print(f"Secret={SECRET_KEY}")
+SECRET_KEY = dotenv_values(dotenv_path=dotenv_file).get("SECRET_KEY", None)
+assert SECRET_KEY is not None
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(dotenv_values(dotenv_path=".env").get("DJANGO_DEBUG", "1"))
+DEBUG = bool(int(dotenv_values(dotenv_path=dotenv_file).get("DJANGO_DEBUG", None)))
 
 ALLOWED_HOSTS = [".openmv.net", "127.0.0.1"]
+
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -89,14 +89,21 @@ if DEBUG:
         }
     }
 else:
+    keys = ["POSTGRES_DB", "POSTGRES_USER", "POSTGRES_PASSWORD", "SQL_HOST", "SQL_PORT"]
+    db_settings = {}
+    for key in keys:
+        value = dotenv_values(dotenv_path=dotenv_file).get(key, None)
+        assert value is not None, f"{key} is not set in the .env file"
+        db_settings[key] = value
+
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB", "db table name here"),
-            "USER": os.environ.get("POSTGRES_USER", "db user name here"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "you guess"),
-            "HOST": os.environ.get("SQL_HOST", "localhost"),
-            "PORT": os.environ.get("SQL_PORT", "5432"),
+            "NAME": db_settings["POSTGRES_DB"],
+            "USER": db_settings["POSTGRES_USER"],
+            "PASSWORD": db_settings["POSTGRES_PASSWORD"],
+            "HOST": db_settings["SQL_HOST"],
+            "PORT": db_settings["SQL_PORT"],
         }
     }
 

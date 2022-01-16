ea8848ea88178880efa08287cfc0b49f34bb95e1
Kevin Dunn <kgdunn@gmail.com>
2022-01-16 10:45:59 +0100

    Debugging in prod

--- openmv/settings.py
+++ openmv/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 from dotenv import dotenv_values, load_dotenv
 
-load_dotenv()
+load_dotenv(filename="/var/django/openmv/.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-
+print(f"Secret={SECRET_KEY}")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(dotenv_values(dotenv_path=".env").get("DJANGO_DEBUG", "1"))
 

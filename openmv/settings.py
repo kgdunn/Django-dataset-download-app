8b1b07d89c15ab8f69202274734f228792b1fe74
Kevin Dunn <kgdunn@gmail.com>
2022-01-16 10:31:40 +0100

    Add in the secret key.

--- openmv/settings.py
+++ openmv/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = dotenv_values(dotenv_path=".env").get("SECRET_KEY", "key here")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(dotenv_values(dotenv_path=".env").get("DJANGO_DEBUG", "1"))
